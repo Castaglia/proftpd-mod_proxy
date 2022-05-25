1cbf479fbd90392c98f79df755c2ffe781bd01d0
TJ Saunders <tj@castaglia.org>
2022-05-24 18:50:35 -0700

    Quell minor compile-time warning about referencing unknown function by actually including the necessary header file.

--- lib/proxy/ssh/service.c
+++ lib/proxy/ssh/service.c
@@ -1,6 +1,6 @@
 /*
  * ProFTPD - mod_proxy SSH service
- * Copyright (c) 2021 TJ Saunders
+ * Copyright (c) 2021-2022 TJ Saunders
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -25,6 +25,7 @@
 #include "mod_proxy.h"
 #include "proxy/ssh/ssh2.h"
 #include "proxy/ssh/packet.h"
+#include "proxy/ssh/service.h"
 
 #if defined(PR_USE_OPENSSL)
 
